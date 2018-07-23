8e0735e91308419a2783942799c9ba904035e6cc
kazuki <kazuki0563@yahoo.co.jp>
2018-07-23 09:27:22 +0900

    second commit

--- testApp/MainActivity.cs
+++ testApp/MainActivity.cs
@@ -1,27 +1,49 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using Java.Interop;
 
 namespace testApp
 {
     [Activity(Label = "testApp", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+
+        TextView textView1;
+        int count;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            //Button button = FindViewById<Button>(Resource.Id.myButton):
+            //button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            FindViewById<Button>(Resource.Id.myButton).Click += (o, e) =>
+                textView1.Text = (++count).ToString();
+            FindViewById<Button>(Resource.Id.button1).Click += (o, e) =>
+            {
+                Toast.MakeText(this, "Hello,Xamarin.Android", ToastLength.Long).Show();
+                textView1.Text = (--count).ToString();
+            };
+            //FindViewById<Button>(Resource.Id.button1) += (o, e) =>
+            //    textView1.Text = (--count).ToString();
+            FindViewById<Button>(Resource.Id.button2).Click += (o, e) =>
+            {
+                var intent = new Intent(this, typeof(SecondActivity));
+                StartActivity(intent);
+            };
+               
         }
+
     }
 }
 
--- testApp/SecondActivity.cs
+++ testApp/SecondActivity.cs
@@ -21,6 +21,7 @@ namespace testApp
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            SetContentView(Resource.Layout.ActivitySecond);
         }
     }
 }
